df016de6310ec907863af1df1731987367d91fcb
Rahul Goyal <rahulgoyal@Rahul-Goyal-SI-0548.local>
2019-07-15 19:47:22 -0400

    removed files

--- subscriber/src/main/java/com/intersect/poc/springcloudsub/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.intersect.poc.springcloudsub.services;
-
-
-import com.intersect.poc.springcloudsub.model.User;
-
-public interface UserService {
-
-    User getUserById(Long userId);
-}
